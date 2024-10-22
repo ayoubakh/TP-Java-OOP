40270d4c8f9c12f753a13f07d1709a7776521a5d
Ayoub Akhadam <56236490+ayoubakh@users.noreply.github.com>
2024-10-23 00:09:10 +0200

    Update Test.java

--- TP3/src/tp3/Test.java
+++ TP3/src/tp3/Test.java
@@ -2,75 +2,45 @@ package tp3;
 
 public class Test {
 
-	public static void main(String[] args) {
-		
-		
-		ResultatModule resModule1 = new ResultatModule(10, 10, 10);
-		ResultatModule resModule2 = new ResultatModule(10, 10, 10);
-		ResultatModule resModule3 = new ResultatModule(1, 3, 10);
-		ResultatModule resModule4 = new ResultatModule(10, 10, 10);
-		
-		
-//		System.out.println(resModule1.getNoteExamen());
-//		
-//		resModule1.setNoteExamen(18);
-//		
-//		System.out.println(resModule1.getNoteExamen());
-//		
-//		System.out.println(resModule1.toString());
-//      System.out.println(resModule1.getId());
-		
-//		System.out.println("La moyenne du 1 er module: "+resModule1.calcMoyenne());
-//		System.out.println(resModule1.valideModuleString());
-//		
-//		System.out.println("La moyenne du 2 eme module: "+resModule2.calcMoyenne());
-//		System.out.println(resModule2.valideModuleString());
-		
-		
-//		ResultatModule[] modulesEtudiant1 = new ResultatModule[20];
-//		
-//		modulesEtudiant1[0] = resModule1;
-//		modulesEtudiant1[1] = resModule2;
-//		modulesEtudiant1[2] = resModule3;
-//		modulesEtudiant1[3] = resModule4;
-//				
-//		AnneeEtudiant etudiant1 =  new AnneeEtudiant(1, 4, modulesEtudiant1);
-//	
-//		System.out.println("Modules Etudiant 1");
-//		
-//		etudiant1.modulesEtuidant();
-//		
-//		System.out.println("-----------------");
-//	    System.out.println("Le nombre total des modules: " +etudiant1.getNombreModules());
-//	    System.out.println("La moyeene de l'annee: " +etudiant1.moyenneAnnee());
-//	    System.out.println("Le nombre du modules valides: " +etudiant1.nombreValides());
-		
-		
-		
-		
-		
-		
-		
-		
-		
-//		System.out.println(resModule1.getNoteExamen());
-//		
-//		resModule1.setNoteExamen(18);
-//		
-//		System.out.println(resModule1.getNoteExamen());
-//		
-//		System.out.println(resModule1.toString());
-		
-//	    System.out.println(ResultatModule.getId());
-		
-//		System.out.println("La moyenne du 1 er module: "+resModule1.calcMoyenne());
-//		System.out.println(resModule1.valideModuleString());
-//		
-//		System.out.println("La moyenne du 2 eme module: "+resModule2.calcMoyenne());
-//		System.out.println(resModule2.valideModuleString());
-		
-		
-
-	}
-
+    public static void main(String[] args) {
+        
+        // Création des instances de ResultatModule pour différents modules
+        ResultatModule resModuleJava = new ResultatModule(10, 10, 10);
+        ResultatModule resModuleUML = new ResultatModule(10, 10, 10);
+        ResultatModule resModuleDatabase = new ResultatModule(1, 3, 10);
+        ResultatModule resModuleC = new ResultatModule(10, 10, 10);
+        
+        // Exemple : Affichage et modification de la note d'examen du module Java
+        System.out.println("La note d'examen du module Java = " + resModuleJava.getNoteExamen());
+        resModuleJava.setNoteExamen(18);
+        System.out.println("La note d'examen du module Java après modification = " + resModuleJava.getNoteExamen());
+        
+        // Calcul de la moyenne du module Java
+        double moyenneModuleJava = resModuleJava.calcMoyenne();
+        System.out.println("La moyenne du module Java = " + moyenneModuleJava);
+        
+        // Vérification de la validation du module Java
+        boolean etatValidationModuleJava = resModuleJava.valideModule();
+        if (etatValidationModuleJava) { // Vérification si le module est validé
+            System.out.println("Le module est validé");
+        } else {
+            System.out.println("Le module n'est pas validé");
+        }
+        
+        // Tableau contenant les résultats des modules pour un étudiant
+        ResultatModule[] modulesEtudiant1 = new ResultatModule[20];
+        modulesEtudiant1[0] = resModuleJava;
+        modulesEtudiant1[1] = resModuleUML;
+        modulesEtudiant1[2] = resModuleDatabase;
+        modulesEtudiant1[3] = resModuleC;
+        
+        // Création d'une instance de AnneeEtudiant pour un étudiant
+        AnneeEtudiant anneeEtudiant1 = new AnneeEtudiant(1, 4, modulesEtudiant1);
+        
+        // Affichage des informations concernant l'année de l'étudiant
+        System.out.println("---------------------------------------------");
+        System.out.println("Le nombre total des modules : " + anneeEtudiant1.getNombreModules());
+        System.out.println("La moyenne de l'année : " + anneeEtudiant1 .moyenneAnnee());
+        System.out.println("Le nombre de modules validés : " + anneeEtudiant1 .nombreValides());
+    }
 }
