db852b147834ffc06d0bfb0271390a82653e373e
Ayoub Akhadam <56236490+ayoubakh@users.noreply.github.com>
2024-10-23 00:12:19 +0200

    Update ResultatModule.java

--- TP3/src/tp3/ResultatModule.java
+++ TP3/src/tp3/ResultatModule.java
@@ -1,93 +1,76 @@
 package tp3;
 
 public class ResultatModule {
-	// les attributs
-	private int id;
-	private double noteCC;
-	private double noteTP;
-	private double noteExamen;
-	
-	private static int compteur=0;
-	
-	// le constructeur 
-	public ResultatModule(double noteCC, double noteTP, double noteExamen) {
-		this.noteCC = noteCC;
-		this.noteTP = noteTP;
-		this.noteExamen = noteExamen;
-		compteur++;
-		this.id = compteur;
-	}
-	
-	// Les accesseurs: Getters and setter
-	// Getter: pour lire l'attribut
-	// setter: pour modifier l'attribut
-	
-	public int getId() {
-		return this.id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public double getNoteCC() {
-		return this.noteCC;
-	}
-	
-	public void setNoteCC(double noteCC) {
-		this.noteCC = noteCC;
-	}
-	
-	public double getNoteTP() {
-		return this.noteCC;
-	}
-	
-	public void setNoteTP(double noteTP) {
-		this.noteTP = noteTP;
-	}
-	
-	public double getNoteExamen() {
-		return this.noteExamen;
-	}
-	
-	public void setNoteExamen(double noteExamen) {
-		this.noteExamen = noteExamen;
-	}
-	
-	
-	// Affichage
-	public String toString() {
-		return "[ResulatModule: NoteCC: "+ this.noteCC
-				+ " NoteTP: "+ this.noteTP
-				+ " NoteExamen: "+ this.noteExamen
-				+ "]"
-				;
-	}
-	
-	
-	 public double calcMoyenne() {
-		return 0.3*this.noteCC + 0.2*this.noteTP + 0.5* this.noteExamen;
-	}
-	
-	 
-	 public Boolean valideModule() {
-		 return calcMoyenne() >= 10;
-	 }
-	 
-	 public String valideModuleString() {
-		 if (this.valideModule() == true) {
-			 return "Le module est validé";
-		 }else {
-			 return "Le module n'est pas validé";
-		 }
-		 
-		 
-	 }
-	 
-	 
-	 
-	
+    // Les attributs
+    private int id;
+    private double noteCC;
+    private double noteTP;
+    private double noteExamen;
+    
+    private static int compteur = 0;
+    
+    // Le constructeur
+    public ResultatModule(double noteCC, double noteTP, double noteExamen) {
+        this.noteCC = noteCC;
+        this.noteTP = noteTP;
+        this.noteExamen = noteExamen;
+        compteur++;
+        this.id = compteur;
+    }
+    
+    // Les accesseurs: Getters et Setters
+    
+    public int getId() {
+        return this.id;
+    }
 
-	
+   // L'id est automatiquement incrémenté, donc nous ne voulons pas le modifier.
+   // Par conséquent, il n'est pas nécessaire d'avoir un setter pour l'id (setId)..
+    
+    public double getNoteCC() {
+        return this.noteCC;
+    }
+    
+    public void setNoteCC(double noteCC) {
+    	this.noteCC = noteCC;
+    }
+    
+    public double getNoteTP() {
+        return this.noteTP;
+    }
+    
+    public void setNoteTP(double noteTP) {
+    	this.noteTP = noteTP;
+    }
+    
+    public double getNoteExamen() {
+        return this.noteExamen;
+    }
+    
+    public void setNoteExamen(double noteExamen) {
+            this.noteExamen = noteExamen;
+    }
+    
+    // Méthode pour calculer la moyenne
+    public double calcMoyenne() {
+        return 0.3 * this.noteCC + 0.2 * this.noteTP + 0.5 * this.noteExamen;
+    }
+    
+    // Vérification de la validation du module
+    public boolean valideModule() {
+        return calcMoyenne() >= 10;
+    }
+    
+
+	@Override
+	public String toString() {
+		return "ResultatModule ["
+				+ "id=" + id 
+				+ ", noteCC=" + noteCC
+				+ ", noteTP=" + noteTP +
+				", noteExamen=" + noteExamen
+				+ "]";
+	}
+    
 
 }
