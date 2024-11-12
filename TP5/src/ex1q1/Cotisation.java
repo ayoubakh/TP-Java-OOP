58fc65fbb75214b5f16732936277527911ccaa96
Ayoub Akhadam <56236490+ayoubakh@users.noreply.github.com>
2024-11-12 21:19:16 +0100

    Delete TP5 directory

--- TP5/src/ex1q1/Cotisation.java
+++
@@ -1,6 +0,0 @@
-package ex1q1;
-
-public interface Cotisation {
-
-	public double calculerCotisation();
-}
--- TP5/src/ex1q2/AbsCotisation.java
+++
@@ -1,6 +0,0 @@
-package ex1q2;
-
-public abstract class AbsCotisation {
-
-    public abstract double calculerCotisation();
-}
